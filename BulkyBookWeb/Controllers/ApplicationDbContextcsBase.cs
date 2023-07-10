56cfb32502de194837a1b47abf1cd2222753e4ea
Mohammad Iman Rezaei <imanri2003@gmail.com>
2023-07-10 10:35:53 +0330

    Initial commit

---
+++ BulkyBookWeb/Controllers/ApplicationDbContextcsBase.cs
@@ -0,0 +1,6 @@
+﻿namespace BulkyBookWeb.Controllers
+{
+    public class ApplicationDbContextcsBase()
+    {
+    }
+}
---
+++ BulkyBookWeb/Data/ApplicationDbContextcs.cs
@@ -0,0 +1,18 @@
+﻿using BulkyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBookWeb.Data
+{
+    public class ApplicationDbContextcs : DbContext
+    {
+        public ApplicationDbContextcs(DbContextOptions<ApplicationDbContextcs> options) : base(options)
+        {
+              // contsrocture that gets DbContext   and send it to base class
+              // with this things (these are for data base)
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+
+    
+}
